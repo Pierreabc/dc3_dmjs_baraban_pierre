bdc878d710682f800280b5f9c1c01c32f485fc66
Pierreabc <99174513+Pierreabc@users.noreply.github.com>
2023-02-05 15:45:18 +0100

    commit 5

---
+++ ex05/ ft_unique_occurences.js
@@ -0,0 +1,10 @@
+function unique(array) {
+    return array.length === new Set(array).size;
+  }
+
+
+        const array2 = [1, 2, 3, 3, 4, 4, 5];
+
+        const result2 = unique(array2);
+
+     console.log(result2);
